3a93c5661bd9f9f09d1247f6956d08f0b25b452c
Julio Cayax <julio1cayax@outlook.es>
2020-06-28 16:05:34 -0600

    Mira si actualizo
    asasas

--- WPFMontecarlo/WPFMontecarlo/MainWindow.xaml.cs
+++ WPFMontecarlo/WPFMontecarlo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFMontecarlo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
